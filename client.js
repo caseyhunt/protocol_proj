df59a52f0d770bca0ea358ec941dea12ad2d4688
caseyhunt <34079427+caseyhunt@users.noreply.github.com>
2020-09-22 17:28:38 -0600

    original app upload
    this is my original prototype of a node and webRTC video calling app

---
+++ client.js
@@ -0,0 +1,166 @@
+//webpage elements
+var divSelectRoom = document.getElementById('selectRoom');
+var divConsultingRoom = document.getElementById("consultingRoom");
+var inputRoomNumber = document.getElementById("roomNumber");
+var btnGoRoom = document.getElementById("goRoom");
+var localVideo = document.getElementById('localVideo');
+var remoteVideo = document.getElementById('remoteVideo');
+
+//global variables
+var roomNumber;
+var localStream;
+var remoteStream;
+var rtcPeerConnection;
+
+//STUN server
+var iceServers={
+  'iceServers':[
+    {'url':'stun:stun.services.mozilla.com'},
+    {'url':'stun:stun.l.google.com:19302'}
+  ]
+}
+var streamConstraints = {audio: true , video: true};
+var isCaller;
+
+//connect to socket server
+var socket = io();
+
+//click event on button
+btnGoRoom.onclick = function(){
+  if(inputRoomNumber.value ===""){
+  alert("Please type a room number");
+}else{
+  roomNumber = inputRoomNumber.value;
+  socket.emit('create or join', roomNumber);
+  divSelectRoom.style = "display:none;"; //don't show display room dialogue (selectRoom div)
+  divConsultingRoom.style = "display:block;"; //show video call interface (consultingRoom div)
+}}
+
+
+//when server gets created, use var streamConstraints to access user media.
+//set localStream html to stream from the user
+//if there's a problem with media, send message to console
+socket.on('created', function (room){
+  navigator.mediaDevices.getUserMedia(streamConstraints).then(function(stream){
+    localStream = stream;
+    // localVideo.src = URL.createObjectURL(stream);
+    localVideo.srcObject = stream;
+    isCaller = true;
+  }).catch(function(err){
+    console.log('An error occurred when accessing media devices')
+  })
+})
+
+//do the same thing as socket.on('created') except for the user joining an existing room
+socket.on('joined', function(room){
+  navigator.mediaDevices.getUserMedia(streamConstraints).then(function(stream){
+    localStream = stream;
+    localVideo.srcObject = stream;
+    socket.emit('ready',roomNumber);
+  }).catch(function(err){
+    console.log('an error occurred when accessing media devices');
+  })
+})
+
+
+
+//webRTC stuff that manages sending the video/audio files in real time
+//this is an API, I don't understand it whatsoever. I got the information for this code from: https://webrtc.ventures/2018/07/tutorial-build-video-conference-application-webrtc-2/
+
+//when server says 'ready'
+socket.on('ready', function(){
+  //creates an RTCPeerConnection object
+  rtcPeerConnection = new rtcPeerConnection(iceServers);
+
+  //adds event listeners to the newly created object
+  rtcPeerConnection.onicecandidate = onIceCandidate;
+  rtcPeerConnection.onaddstream = onAddStream;
+
+  //adds current local stream to the object
+  rtcPeerConnection.addStream(localStream);
+
+  //prepares an Offer
+  rtcPeerConnection.createOffer(setLocalAndOffer, function(e){
+    console.log(e)
+  });
+
+});
+
+//when server emits Offer
+socket.on('offer', function(event){
+  if(!isCaller){//is caller is set to true in on socket 'created'
+    //creates an rtcPeerConnection object
+    rtcPeerConnection = new RTCPeerConnection(iceServers);
+
+    //adds event listeners to the newly created created object
+    rtcPeerConnection.onicecandidate = onIceCandidate;
+    rtcPeerConnection.onaddstream = onAddStream;
+
+    //adds the current local stream to the object
+    rtcPeerConnection.addStream(localStream);
+
+    //stores the offer as remote description
+    rtcPeerConnection.setRemoteDescription(new RTCSessionDescription(event));
+
+    //prepares an answer
+    rtcPeerConnection.createAnswer(setLocalAndAnswer, function(e){console.log(e)});
+    }
+});
+
+//when server emits answer
+socket.on('answer', function(event){
+  //stores it as remote description
+  rtcPeerConnection.setRemoteDescription(new RTCSessionDescription(event));;
+})
+
+//when server emits candidate
+socket.on('candidate', function(event){
+  //creates a candidate object
+  var candidate = new RTCIceCandidate({
+    sdpMLineIndex:event.label,
+    candidate:event.candidate
+  });
+  //stores candidate
+  rtcPeerConnection.addIceCandidate(candidate);
+});
+
+
+//display the other user's video!
+function onAddStream(event){
+  remoteVideo.srcObject = stream;
+  remoteStream = event.stream;
+}
+
+//sends a candidate message to the server
+function onIceCandidate(event){
+  if(event.candidate){
+    console.log('sending ice candidate');
+    socket.emit('candidate', {
+      type: 'candidate',
+      label: event.candidate.sdpMLineIndex,
+      id: event.candidate.sdpMid,
+      candidate: event.candidate.candidate,
+      room: roomNumber
+    })
+  }
+}
+
+//stores offer and sends message to server
+function setLocalAndOffer(sessionDescription){
+  rtcPeerConnection.setLocalDescription(sessionDescription);
+  socket.emit('offer',{
+    type:'offer',
+    sdp: sessionDescription,
+    room: roomNumber
+  });
+}
+
+//stores answer and sends message to server
+function setLocalAndAnswer(sessionDescription){
+  rtcPeerConnection.setLocalDescription(sessionDescription);
+  socket.emit('answer',{
+    type:'answer',
+    sdp:sessionDescription,
+    room: roomNumber
+  });
+}
---
+++ server.js
@@ -0,0 +1,63 @@
+var helmet = require('helmet');
+
+const express = require('express');
+
+// const app = express();
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+
+
+app.get('/', (req,res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.use(express.static(__dirname))
+app.use(express.static('public'));
+
+
+//this stuff all connects to the client
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.on('create or join', function(room){
+    console.log('create or join to room ', room);
+
+    //count users in room
+    var myRoom = io.sockets.adapter.rooms[room] || {length:0};
+    var numClients = myRoom.length;
+    console.log(room, ' has ', numClients, ' clients');
+
+    if (numClients == 0){
+        socket.join(room);
+        socket.emit('created',room);
+    }else if(numClients == 1){
+      socket.join(room);
+      socket.emit('joined', room);
+    }else{
+      socket.emit('full',room)
+    }
+  });
+
+  socket.on('ready', function(room){
+    socket.broadcast.to(room).emit('ready');
+  });
+
+  socket.on('candidate', function(event){
+    socket.broadcast.to(event.room).emit('offer',event.sdp);
+  });
+
+  socket.on('answer', function(event){
+    socket.broadcast.to(event.room).emit('answer',event.sdp);
+  });
+});
+
+//listener
+// http.listen(3000, () =>{
+//   console.log('listening on *:3000');
+// });
+
+http.listen(5500, () =>{
+  console.log('listening on *:5500');
+});

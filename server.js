1493d985ef09eef84fec4453685ec714aba0957b
caseyhunt <34079427+caseyhunt@users.noreply.github.com>
2020-09-28 14:15:30 -0600

    Update server.js

--- server.js
+++ server.js
@@ -43,4 +43,4 @@ if (process.env.PROD){
 }
 
 const port = process.env.PORT || 8000;
-server.listen(port, () => console.log('server is running on port: ${port}'));
+server.listen(port, () => console.log('server is running on port: '+port));

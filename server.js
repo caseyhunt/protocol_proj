6340364dff4b8c5573da3778e558c561cfb962c5
caseyhunt <34079427+caseyhunt@users.noreply.github.com>
2020-09-28 18:21:16 -0600

    Update server.js

--- server.js
+++ server.js
@@ -36,9 +36,9 @@ io.on("connection", socket => {
     });
 });
  // if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(_dirname, './client/build')));
+  app.use(express.static(path.join(__dirname, './client/build')));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(_dirname, './client/build/index.html'));
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
   });
 //}
 

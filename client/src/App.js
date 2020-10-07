e7b6529aad9b4709177dec5680a7c21b082688c8
caseyhunt <34079427+caseyhunt@users.noreply.github.com>
2020-10-07 02:36:07 -0600

    Removing test routing because I solved dynamic render puzzle

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import CreateRoom from "./routes/CreateRoom";
 import Room from "./routes/Room";
-import Test from "./routes/test";
 import './App.css';
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route path="/" exact component={CreateRoom} />
-          <Route path="/test/" component={Test}/>
           <Route path="/room/:roomID" component={Room}/>
         </Switch>
       </BrowserRouter>
